248fd414e6e7238cf79be3d41b8e8c941fc11f39
Bbabiker <Bbabiker@asu.edu>
2022-03-24 22:41:30 -0700

    Add test cases

--- src/test/java/BlackBoxGiven.java
+++ src/test/java/BlackBoxGiven.java
@@ -47,12 +47,14 @@ public class BlackBoxGiven {
         return constructor.newInstance(age);
     }
 
-    // A sample Cart
+   // A sample Cart
 
-    Cart cart1;
-    double cart1Expected;
+    Cart cart1, cart2, cart3, cart4, cart5, cart6;
+    double cart1Expected, cart2Expected, cart3Expected, cart4Expected, cart5Expected, cart6Expected;
 
 
+    
+    // check for Alcohol+Frozen along with other items
     @org.junit.Before
     public void setUp() throws Exception {
 
@@ -62,6 +64,7 @@ public class BlackBoxGiven {
         cart1 = createCart(40);
         for (int i = 0; i < 2; i++) {
             cart1.addItem(new Alcohol());
+            
         }
         for(int i = 0; i < 3; i++) {
             cart1.addItem(new Dairy());
@@ -72,11 +75,188 @@ public class BlackBoxGiven {
 
         cart1Expected = 70.2;
     }
+    
+    
+    // test when atlest one item from each product is in the cart
+    @org.junit.Before
+    public void setUp2() throws Exception {
 
-    // sample test
+        
+        // cart created with an age 40 shopper
+        cart2 = createCart(40);
+        
+        cart2.addItem(new Alcohol());
+     
+        cart2.addItem(new Dairy());
+      
+        cart2.addItem(new Meat());
+        
+        cart2.addItem(new Produce());
+        cart2.addItem(new FrozenFood());
+
+        cart2Expected = 27;
+    }
+    
+    
+    
+    
+     // test when atlest one dicount is applicaple for items in the cart
+    @org.junit.Before
+    public void setUp3() throws Exception {
+
+       
+        
+         cart3 = createCart(40);
+         
+        for (int i = 0; i < 2; i++) {
+            cart3.addItem(new Alcohol());
+       }
+       for(int i = 0; i < 2; i++) {
+            cart3.addItem(new Dairy());
+        }
+       for(int i = 0; i < 2; i++) {
+            cart3.addItem(new Meat());
+       }
+       
+       for(int i = 0; i < 2; i++) {
+        cart3.addItem(new Produce());
+       }
+       
+       for(int i = 0; i < 2; i++) {
+        cart3.addItem(new FrozenFood());
+       }
+        
+        
+       cart3Expected = 54;
+    }
+    
+
+    // check for large number of items in the cart and if a discount is applicaple
+     @org.junit.Before
+    public void setUp4() throws Exception {
+
+        
+         cart4 = createCart(40);
+         
+        for (int i = 0; i < 100; i++) {
+            cart4.addItem(new Alcohol());
+       }
+       
+       for(int i = 0; i < 100; i++) {
+            cart4.addItem(new Meat());
+       }
+       
+       for(int i = 0; i < 100; i++) {
+        cart4.addItem(new Produce());
+       }
+       
+       for(int i = 0; i < 100; i++) {
+        cart4.addItem(new FrozenFood());
+       }
+        
+         cart4Expected = 2340.36;
+    }
+    
+    
+    // check for Alcohol age restruction
+     @org.junit.Before
+    public void setUp5() throws Exception {
+
+        // cart created with an age 21 shopper
+        cart5 = createCart(21);
+        
+        cart5.addItem(new Alcohol());
+       
+
+        cart5Expected = 8.64;
+    }
+    
+    
+    // check for Alcohol age restruction
+      @org.junit.Before
+    public void setUp6() throws Exception {
+
+         // cart created with an age 20 shopper
+        cart6 = createCart(20);
+       
+        cart6.addItem(new Alcohol());
+      
+
+        cart6Expected = 0;
+    }
+    
+    
+    
+    
+
+    // check for Alcohol+Frozen along with other items
     @Test
     public void calcCostCart1() throws UnderAgeException {
+        
         double amount = cart1.calcCost();
+        
         assertEquals(cart1Expected, amount, .01);
+       
     }
+    
+    
+    // test when atlest one item from each product is in the cart
+    // used EP and BVA
+    @Test
+    public void calcCostCart2() throws UnderAgeException {
+        
+        double amount = cart2.calcCost();
+        
+        assertEquals(cart2Expected, amount, .01);
+        
+        
+    }
+    
+     // test when atlest one dicount is applicaple for items in the cart
+    // used EP and BVA
+    @Test
+    public void calcCostCart3() throws UnderAgeException {
+        
+        double amount = cart3.calcCost();
+        
+        assertEquals(cart3Expected, amount, .01);
+    
+       
+    }
+    
+    
+    // check for large number of items in the cart and if a discount is applicaple
+    //used EP and BVA
+    @Test
+    public void calcCostCart4() throws UnderAgeException {
+       
+        double amount = cart4.calcCost();
+        
+        assertEquals(cart4Expected, amount, .01);
+    }
+    
+    
+    // check for Alcohol age restruction
+    //used EP and BVA
+    @Test
+    public void calcCostCart5() throws UnderAgeException {
+       
+        double amount = cart5.calcCost();
+       
+        assertEquals(cart5Expected, amount, .01);
+    }
+    
+    
+    // check for Alcohol age restruction
+    // used EP and BVA
+    @Test
+    public void calcCostCart6() throws UnderAgeException {
+        
+        double amount = cart6.calcCost();
+        
+        assertEquals(cart6Expected, amount, .01);
+    }
+    
+    
+    
 }
