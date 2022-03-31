4510f385e66bca39c2ad5a96b76576eb1d036d1d
Bbabiker <Bbabiker@asu.edu>
2022-03-30 23:01:38 -0700

    Fix defect Id# 3
    - Change the access modifer of the attribute cart in class Cart.java from public to protecded to conform with the coding standard document.
    -GitHub refrence: issue # 3

--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Cart {
 
     protected int userAge;
-    public List<Product> cart;
+    protected List<Product> cart;  // change access modifier from public to protected
     public int cartStorage;
 
     /**
